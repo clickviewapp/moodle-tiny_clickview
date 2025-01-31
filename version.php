4b5e2ff450673bca38d593cbc1bc26b3f5ef979b
Sha <shale.kuzmanovski@clickview.com.au>
2025-01-31 15:59:42 +1100

    Bumping version, release and deps

--- version.php
+++ version.php
@@ -24,12 +24,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023051402;
+$plugin->version = 2023051403;
 $plugin->requires = 2022112800;
 $plugin->component = 'tiny_clickview';
 $plugin->supported = [401, 405];
-$plugin->release = 'v4.1-r2 - v4.5-r1';
+$plugin->release = 'v4.1-r3 - v4.5-r2';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'local_clickview' => 2021083003,
+    'local_clickview' => 2021083005,
 ];
